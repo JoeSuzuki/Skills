9dd21bc42077329a2ae55db8cb1359492c7ecac0
JoeSuzuki <joesuzuki516@gmail.com>
2018-04-07 22:26:10 -0400

    redview in profile

--- Skills/Skills/Controllers/ProfileViewController.swift
+++ Skills/Skills/Controllers/ProfileViewController.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2018 Joe Suzuki. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        view.addSubview(redView)
+        
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    
+}
+
