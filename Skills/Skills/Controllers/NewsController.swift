55a46cc9857f3b040a8fc0dbfcffe7b6d3918ce3
JoeSuzuki <joesuzuki516@gmail.com>
2018-04-08 03:51:59 -0400

    newsController

---
+++ Skills/Skills/Controllers/NewsController.swift
@@ -0,0 +1,51 @@
+//
+//  NewsController.swift
+//  Skills
+//
+//  Created by Joe Suzuki on 4/8/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class NewsController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    let headerId = "headerId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        collectionView?.backgroundColor = UIColor.main.mainColor
+        collectionView?.register(LeftBlockDisplayCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(HomeHeaderCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        
+        setupNavigationBarItems()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 260)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,withReuseIdentifier: headerId, for: indexPath)
+        header.backgroundColor = .white
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 40)
+    }
+}
+
+
--- Skills/Skills/Views/LeftBlockDisplayCell.swift
+++ Skills/Skills/Views/LeftBlockDisplayCell.swift
@@ -14,16 +14,36 @@ class LeftBlockDisplayCell: UICollectionViewCell {
         super.init(frame: frame)
         setupViews()
     }
-    let backgroundColorView: UIView = {
+    let redView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let blueView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let greenView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     func setupViews() {
-        addSubview(backgroundColorView)
-            
+        [redView, blueView, greenView].forEach {addSubview($0)}
+        
+        redView.anchor(top: self.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: self.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: self.layer.frame.width / 3, height: 0))
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+        
+        blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
+        blueView.anchorSize(to: redView)
+        
+        greenView.anchor(top: redView.topAnchor, leading: self.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
+
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
